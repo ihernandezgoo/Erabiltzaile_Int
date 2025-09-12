007e6c2636a1c551049c9f202e699af23275f8e8
ihernandezgoo <ihernandezgo25paag@ikzubirimanteo.com>
2025-09-12 13:13:51 +0200

    5. Ariketa bukatuta

---
+++ 1. UD_Hasiera/5. Ariketa/5. Ariketa/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace _5._Ariketa
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComicsSans(object sender, RoutedEventArgs e)
+        {
+            Testua1.FontFamily = new FontFamily("Comic Sans MS");
+        }
+
+        private void Negrita(object sender, RoutedEventArgs e)
+        {
+            Testua1.FontWeight = FontWeights.Bold;
+        }
+
+        private void Tachado(object sender, RoutedEventArgs e)
+        {
+            Testua1.TextDecorations = TextDecorations.Strikethrough;
+        }
+
+        private void TamañoMas(object sender, RoutedEventArgs e)
+        {
+            Testua1.FontSize += 2;
+        }
+
+        private void TamañoMenos(object sender, RoutedEventArgs e)
+        {
+            if (Testua1.FontSize > 2)
+                Testua1.FontSize -= 2;
+        }
+
+        private void Courtier(object sender, RoutedEventArgs e)
+        {
+            Testua1.FontFamily = new FontFamily("Courier New");
+        }
+
+        private void Cursiva(object sender, RoutedEventArgs e)
+        {
+            Testua1.FontStyle = FontStyles.Italic;
+        }
+
+        private void Subrayado(object sender, RoutedEventArgs e)
+        {
+            Testua1.TextDecorations = TextDecorations.Underline;
+        }
+    }
+}
