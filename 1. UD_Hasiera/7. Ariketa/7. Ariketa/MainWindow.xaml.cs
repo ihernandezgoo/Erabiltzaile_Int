31ff9a13fc38c74fd7a58c55db5d73ee62d2c9f3
ihernandezgoo <ihernandezgo25paag@ikzubirimanteo.com>
2025-09-15 12:31:47 +0200

    Kalkulagailua bukatuta

--- 1. UD_Hasiera/7. Ariketa/7. Ariketa/MainWindow.xaml.cs
+++ 1. UD_Hasiera/7. Ariketa/7. Ariketa/MainWindow.xaml.cs
@@ -1,19 +1,11 @@
 ﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Data;
 
 namespace _7._Ariketa
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -21,6 +13,39 @@ namespace _7._Ariketa
             InitializeComponent();
         }
 
-        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                Erantzuna.Text += btn.Content.ToString();
+            }
+        }
+
+        private void C_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Erantzuna.Text))
+            {
+                Erantzuna.Text = Erantzuna.Text.Substring(0, Erantzuna.Text.Length - 1);
+            }
+        }
+
+        private void CE_Click(object sender, RoutedEventArgs e)
+        {
+            Erantzuna.Text = string.Empty;
+        }
+
+        private void Berdin_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string expr = Erantzuna.Text.Replace("%", "/100.0*");
+                var result = new DataTable().Compute(expr, null);
+                Erantzuna.Text = result.ToString();
+            }
+            catch
+            {
+                Erantzuna.Text = "Errorea";
+            }
+        }
     }
 }
