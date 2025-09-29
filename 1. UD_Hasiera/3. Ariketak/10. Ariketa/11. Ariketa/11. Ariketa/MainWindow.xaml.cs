36cf45e6d1b70f459484bcb42c0af55b68a1fbb6
ihernandezgoo <ihernandezgo25paag@ikzubirimanteo.com>
2025-09-29 13:38:39 +0200

    Karpetak eguneratutak

---
+++ 1. UD_Hasiera/3. Ariketak/10. Ariketa/11. Ariketa/11. Ariketa/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace _11._Ariketa
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Onartu_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Datuak gorde dira (opcional).");
+        }
+
+        private void Begiratu_Click(object sender, RoutedEventArgs e)
+        {
+            lblBienvenida.Content = "Ongietorri sistemara\n" + "\n" +
+                                    txtIzena.Text + " " +
+                                    txtAbizena1.Text + " " +
+                                    txtAbizena2.Text + "\n" +
+                                    txtNan.Text;
+
+            PanelFormulario.Visibility = Visibility.Collapsed;
+            PanelVisualizar.Visibility = Visibility.Visible;
+        }
+
+        private void Salir_Click(object sender, RoutedEventArgs e)
+        {
+            PanelVisualizar.Visibility = Visibility.Collapsed;
+            PanelFormulario.Visibility = Visibility.Visible;
+        }
+
+        private void Atera_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
