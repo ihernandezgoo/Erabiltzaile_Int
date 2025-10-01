30e86aff67d65a9b2b4bb91d04fcf8368af53728
ihernandezgoo <ihernandezgo25paag@ikzubirimanteo.com>
2025-10-01 14:24:11 +0200

    13. Ariketa bukatuta

--- 1. UD_Hasiera/3. Ariketak/13. Ariketa/13. Ariketa/13. Ariketa/MainWindow.xaml.cs
+++ 1. UD_Hasiera/3. Ariketak/13. Ariketa/13. Ariketa/13. Ariketa/MainWindow.xaml.cs
@@ -1,24 +1,32 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace _13._Ariketa
+namespace TestuEditorea
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Cut, (s, e) => editor.Cut()));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Copy, (s, e) => editor.Copy()));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Paste, (s, e) => editor.Paste()));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Delete, (s, e) => editor.SelectedText = ""));
         }
+
+        private void Irten_Click(object sender, RoutedEventArgs e) => Close();
+
+        private void Moztu_Click(object sender, RoutedEventArgs e) => editor.Cut();
+        private void Kopiatu_Click(object sender, RoutedEventArgs e) => editor.Copy();
+        private void Itsatsi_Click(object sender, RoutedEventArgs e) => editor.Paste();
+        private void Ezabatu_Click(object sender, RoutedEventArgs e) => editor.SelectedText = "";
+
+        private void Arial_Click(object sender, RoutedEventArgs e) => editor.FontFamily = new FontFamily("Arial");
+        private void Courier_Click(object sender, RoutedEventArgs e) => editor.FontFamily = new FontFamily("Courier New");
+        private void Impact_Click(object sender, RoutedEventArgs e) => editor.FontFamily = new FontFamily("Impact");
+        private void Symbol_Click(object sender, RoutedEventArgs e) => editor.FontFamily = new FontFamily("Symbol");
     }
 }
